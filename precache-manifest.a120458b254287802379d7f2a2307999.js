2e1a473e76d743c72838360df4dc223f417fb48d
foxxycat <43220582+foxxycat@users.noreply.github.com>
2020-07-19 19:30:20 +0300

    Updates

---
+++ precache-manifest.a120458b254287802379d7f2a2307999.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a6e2bf8f7a596b5798f4b0fc0ad14f0",
+    "url": "/dnd/index.html"
+  },
+  {
+    "revision": "698d35f11f226e4332e7",
+    "url": "/dnd/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "0babbde14e333bc689a2",
+    "url": "/dnd/static/js/2.8aaac6e4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/dnd/static/js/2.8aaac6e4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "698d35f11f226e4332e7",
+    "url": "/dnd/static/js/main.e63b282b.chunk.js"
+  },
+  {
+    "revision": "7b594acb712a9fd65048",
+    "url": "/dnd/static/js/runtime-main.b939fdb8.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/dnd/static/media/logo.5d5d9eef.svg"
+  }
+]);
